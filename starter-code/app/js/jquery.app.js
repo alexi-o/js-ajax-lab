e8d21b464a87d9d3780c9c6e3cb6322d9d16310b
AlexiOHearn <alexiohearn@o.local>
2017-04-03 20:08:59 -0600

    gets ajax working, but form doesnt clear after submit

--- starter-code/app/js/jquery.app.js
+++ starter-code/app/js/jquery.app.js
@@ -1,10 +1,43 @@
-var catList = $('#cats');
-
-var ajax = $.get('https://ga-cat-rescue.herokuapp.com/api/cats')
-    .done(function(data){
-      console.log(data);
-      var catsTwo = JSON.parse(data)
-      console.log(catsTwo);
-      catList.append(catsTwo);
-    });
-
+$("document").ready(function() {
+ 	var catList = document.getElementById('cats');
+	function makeCats(data) {
+		for(var i = 0; i < data.length; i++) {
+			var cats = document.createElement("li");
+			cats.innerHTML = (data[i].name + " - " + data[i].note);
+			catList.appendChild(cats);
+		}
+	}
+	function createCat(name, note) {
+		var cat = {
+			"name": name,
+			"note": note
+		}
+		$.ajax({
+			type: "POST",
+			url: "https://ga-cat-rescue.herokuapp.com/api/cats",
+			data: JSON.stringify(cat)
+		}).done(function() {
+			$.get("https://ga-cat-rescue.herokuapp.com/api/cats", function(data){
+				var newCats = JSON.parse(data);
+				var newCat = newCats[newCats.length-1];
+				var name = newCat.name;
+				var note = newCat.note;
+				var cat = document.createElement("li");
+				cat.innerHTML = (name + " - " + note);
+				catList.prepend(cat);
+			})
+		});
+	}
+	$.ajax({
+		url: "https://ga-cat-rescue.herokuapp.com/api/cats"
+		}).done(function(data) {
+		var catData = JSON.parse(data);
+		makeCats(catData);
+	});
+	$("#new-cat").on("submit", function(e) {
+		e.preventDefault();
+		var name = $("#cat-name").val();
+		var note = $("#cat-note").val();
+		createCat(name, note);
+	});
+});
